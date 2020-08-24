75884757b22bfc484e485a4e9be804c2db07b397
barbatoz0220 <47182649+barbatoz0220@users.noreply.github.com>
2020-08-24 20:59:01 +0700

    This commit adds a sample user's post on the main feed. Will work on the looks later.

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import './Post.css';
+import React from "react";
+import "./Post.css";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
 
-function Post() {
+function Post({ displayName, username, verified, text, image, avatar }) {
     return (
-        <div className="postComponent">
-            
+        <div className="post">
+            <div className="post__avatar">
+                <Avatar></Avatar>
+            </div>
+            <div className="post__body">
+                <div className="post__header">
+                    <div className="post__headerText">
+                        <h3>
+                            Other user{" "}
+                            <span>
+                                <VerifiedUserIcon className="post__badge" /> @otheraccount
+                            </span>
+                        </h3>
+                    </div>
+                    <div className="post__headerDescription">
+                        <p>I challenge you to a Twitter Clone Development</p>
+                    </div>
+                </div>
+                <img src="http://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif" alt=""/>
+            </div>
+            <div className="post__footer">
+                <ChatBubbleOutlineIcon fontSize="small" />
+                <RepeatIcon fontSize="small" />
+                <FavoriteBorderIcon fontSize="small" />
+                <PublishIcon fontSize="small" />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Post
+export default Post;
